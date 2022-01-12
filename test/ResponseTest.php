1c0b13468cfaaa7c5d924af55dcdeba6c816e338
Benedikt Jazdzewski <jazdzewski@b1-systems.de>
2022-01-12 16:02:54 +0100

    unittests for  Responses.php

---
+++ test/ResponseTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Horde\Http\Test;
+
+use Phpunit\Framework\TestCase;
+use Horde\Http\Response;
+use Psr\Http\Message\ResponseInterface;
+use InvalidArgumentException;
+
+class ResponseTest extends TestCase
+{
+    public function setUp(): void
+    {
+    }
+
+    public function testDefaultStatusIs200()
+    {
+        $response = new Response();
+        $status = $response->getStatusCode();
+        $this -> assertSame(200, $status);
+    }
+
+    public function testExceptionWhenUnkownStatusCode()
+    {
+        $response = new Response();
+        $this -> expectException(InvalidArgumentException::class);
+        $response->withStatus(999);
+
+    }
+
+    public function testStatusChangesAccordingly()
+    {
+        $response = new Response(418);
+        $status = $response->getStatusCode();
+        $this -> assertSame(418, $status);
+    }
+
+    public function testReasonPhraseIsDefault()
+    {
+        $response = new Response(418,[],null,'1.1');
+        $responsePhrase = $response->getReasonPhrase();
+        $this -> assertSame("I'm a teapot", $responsePhrase);
+    }
+
+    public function testReasonPhraseIsPrioritised()
+    {
+        $response = new Response(418,[],null,'1.1',"I'm a teapot");
+        $responsePhrase = $response->getReasonPhrase();
+        $this -> assertSame("I'm a teapot", $responsePhrase);
+    }
+
+    public function testwithStatusReturnsAccordinglyToRFC7231()
+    {
+        $response = new Response();
+        $responsePhrase = $response->withStatus(418, 'test me if you can')->getReasonPhrase();
+        $this -> assertSame("I'm a teapot", $responsePhrase);
+    }
+
+    public function testwithStatusPrioritisedSpecificResponsePhrase()
+    {
+        $response = new Response();
+        $responsePhrase = $response->withStatus(418, 'test me if you can')->getReasonPhrase();
+        $this -> assertSame('test me if you can', $responsePhrase);
+    }
+}
