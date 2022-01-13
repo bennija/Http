19e14a5148eb0756642be1bda321f046fb343d18
Benedikt Jazdzewski <jazdzewski@b1-systems.de>
2022-01-13 12:22:46 +0100

    resolved some problems but test for getSize trows Error

--- test/StreamTest.php
+++ test/StreamTest.php
@@ -17,7 +17,7 @@ class StreamTest extends TestCase
     {
         $stream = new Stream(fopen('php://temp', 'r'));
         $isReadable = $stream->isSeekable();
-        $this -> assertSame(true, $isReadable);;
+        $this->assertSame(true, $isReadable);;
     }
 
     public function testIsNotSeekable()
@@ -25,12 +25,12 @@ class StreamTest extends TestCase
         $stream = new Stream(fopen('php://temp', 'r'));
         $stream->close();
         $isReadable = $stream->isSeekable();
-        $this -> assertSame(false, $isReadable);;
+        $this->assertSame(false, $isReadable);;
     }
 
     public function testExceptionWhenNoResource()
     {
-        $this -> expectException(InvalidArgumentException::class);
+        $this->expectException(InvalidArgumentException::class);
         $stream = new Stream('test');
     }
 
@@ -38,76 +38,118 @@ class StreamTest extends TestCase
     {
         $stream = new Stream(fopen('php://temp', 'r+'));
         $isWritable = $stream->isWritable();
-        $this -> assertSame(true, $isWritable);
+        $this->assertSame(true, $isWritable);
     }
 
     public function testWriteToString()
     {
         $stream = new Stream(fopen('php://temp', 'r+'));
-        $stream->write('test');
+        $stream->write('123456789TestXYZ');
         $toString = $stream->__toString();
-        $this -> assertSame('test', $toString);
+        $this->assertSame('123456789TestXYZ', $toString);
     }
 
-    //TODO getSize() returns null
+    public function testWriteToStringWithUnseekable()
+    {
+        $stream = new Stream(fopen('php://temp', 'a+'));
+        $stream->write('123456789TestXYZ');
+        $toString = $stream->__toString();
+        $this->assertSame('123456789TestXYZ', $toString);
+    }
+
     public function testGetSize()
     {
         $stream = new Stream(fopen('php://temp', 'r+'));
-        $stream->write('test');
+        $stream->write('123456789TestXYZ');
         $size = $stream->getSize();
-        $this -> assertSame(null, $size);
+        $this->assertSame(null, $size);
+    }
+
+    public function testTellImmediately()
+    {
+        $stream = new Stream(fopen('php://temp', 'r+'));
+        $tell = $stream->tell();
+        $this->assertSame(0, $tell);;
     }
 
     public function testTell()
     {
         $stream = new Stream(fopen('php://temp', 'r+'));
-        $stream->write('test');
+        $stream->write('123456789TestXYZ');
         $tell = $stream->tell();
-        $this -> assertSame(4, $tell);;
+        $this->assertSame(16, $tell);;
     }
 
     public function testWriteReadAndEof()
     {
         $stream = new Stream(fopen('php://temp', 'r+'));
-        $stream->write('test');
-        $stream->read(4);
+        $stream->write('123456789TestXYZ');
+        $stream->read(16);
         $eof = $stream->eof();
-        $this -> assertSame(true, $eof);
+        $this->assertSame(true, $eof);
     }
 
-    //Todo does this test make any sense
     public function testSeek()
     {
         $stream = new Stream(fopen('php://temp', 'r+'));
-        $stream->write('test');
-        $seek = $stream->seek(4);
-        $this -> assertSame(null, $seek);
+        $stream->write('123456789TestXYZ');
+        $stream->seek(9);
+        $seeked = $stream->read(4);
+        $this->assertSame("Test", $seeked);
     }
 
     public function testRewind()
     {
         $stream = new Stream(fopen('php://temp', 'r+'));
-        $stream->write('test');
+        $stream->write('123456789TestXYZ');
         $stream->read(4);
         $stream->rewind();
-        $eof = $stream->eof();
-        $this -> assertSame(false, $eof);
+        $rewinded = $stream->read(9);
+        $this->assertSame('123456789', $rewinded);
     }
 
-    //Todo getContents allways returns ''
     public function testGetContents()
     {
         $stream = new Stream(fopen('php://temp', 'r+'));
-        $stream->write('test');
+        $stream->write('123456789TestXYZ');
         $stream->read(2);
         $content = $stream->getContents();
-        $this -> assertSame('st', $content);
+        $this->assertSame('', $content);
     }
 
     public function testGetMetadata()
     {
         $stream = new Stream(fopen('php://temp', 'r+'));
         $meta = $stream->getMetadata('wrapper_type');
-        $this -> assertSame('PHP', $meta);
+        $this->assertSame('PHP', $meta);
+    }
+
+    public function testGetMetadataInvalidKey()
+    {
+        $stream = new Stream(fopen('php://temp', 'r+'));
+        $meta = $stream->getMetadata('invalid_key');
+        $this->assertSame(null, $meta);
+    }
+
+    public function testGetMetadataFullArray()
+    {
+        $stream = new Stream(fopen('php://temp', 'r+'));
+        $meta = $stream->getMetadata();
+        $this->assertSame([
+            'wrapper_type' => 'PHP',
+            'stream_type' => 'TEMP',
+            'mode' => 'w+b',
+            'unread_bytes' => 0,
+            'seekable' => true,
+            'uri' => 'php://temp',
+        ], $meta);
+    }
+
+    public function testDetach()
+    {
+        $stream = new Stream(fopen('php://temp', 'r+'));
+        $stream->detach();
+        $isReadable = $stream->isReadable();
+        $this->assertSame(false, $isReadable);
     }
 }
